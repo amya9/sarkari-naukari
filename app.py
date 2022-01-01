eafd3f2d162b9cca86b69759d0386952ca7dbed8
amya9 <amitanand9799@gmail.com>
2022-01-01 21:47:44 +0530

    secret

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import random
 
 
 app = Flask(__name__)
-app.secret_key= "amit kumar"
+SESSION_TYPE = 'mongodb'
+app.secret_key=os.urandom(24)
 # load_dotenv() # use dotenv to hide sensitive credential as environment variables
 # mongo_client = PyMongo(app , uri="mongodb+srv://username:password@cluster0.h1dw4.mongodb.net/category?retryWrites=true&w=majority".format(username , password))
 app.config["MONGO_URI"] = "mongodb+srv://amya:amit9799@cluster0.h1dw4.mongodb.net/myAdmin?retryWrites=true&w=majority"
